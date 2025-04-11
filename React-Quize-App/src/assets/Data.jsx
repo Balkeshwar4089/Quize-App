d404fdd9be414e57ef4d6289c2a23cd92dc46578
Balkeshwar <ac4370879@gmail.com>
2025-04-11 15:37:01 +0530

    Add files via upload

---
+++ React-Quize-App/src/assets/Data.jsx
@@ -0,0 +1,64 @@
+export let Data = [
+  {
+    question: "What temperature does water boil at?",
+    option1: "50 degrees Celcius",
+    option2: "25 degrees Celcius",
+    option3: "100 degrees Celcius",
+    option4: "150 degrees Celcius",
+    ans: 3,
+  },
+
+  {
+    question: "Which is the only mammal that can’t jump?",
+    option1: "Dog",
+    option2: "Elephant",
+    option3: "Goat",
+    option4: "Lion",
+    ans: 2,
+  },
+
+  {
+    question: "What colour is a panda?",
+    option1: "Green and Yellow",
+    option2: "Blue and Red",
+    option3: "Green and White",
+    option4: "Black and White",
+    ans: 4,
+  },
+
+  {
+    question: "Where is the smallest bone in the human body?",
+    option1: "The Chest",
+    option2: "The Ear",
+    option3: "The Legs",
+    option4: "The Hands",
+    ans: 2,
+  },
+
+  {
+    question: "What does the roman numeral C represent?",
+    option1: "100",
+    option2: "10",
+    option3: "10,000",
+    option4: "1,000,000",
+    ans: 1,
+  },
+
+  {
+    question: "How many squares are there on a chess board?",
+    option1: "128",
+    option2: "64",
+    option3: "32",
+    option4: "256",
+    ans: 2,
+  },
+
+  {
+    question: "Who invented the electric light bulb?",
+    option1: "Tom Cruise",
+    option2: "Barack Obama",
+    option3: "Wole Soyinka",
+    option4: "Thomas Edison",
+    ans: 4,
+  },
+];
